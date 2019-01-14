eca76ddf6545c0e34c9b8d436b3718bad1e2f46c
Logan Elliott <illusivetea@outlook.com>
2019-01-14 22:47:13 +0000

    Add Files

---
+++ discord_perms/config.lua
@@ -0,0 +1,9 @@
+Config = {
+	DiscordToken = "Bot Discord Token Here",
+	GuildId = "Guild ID Here",
+
+	-- Format: ["Role Nickname"] = "Role ID" You can get role id by doing \@RoleName
+	Roles = {
+		["TestRole"] = "Some Role ID"
+	}
+}

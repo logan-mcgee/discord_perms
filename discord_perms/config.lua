76866d43b2047b3ff2b5461b020a9e6a628f6652
Logan Elliott <illusivetea@outlook.com>
2019-01-14 22:58:44 +0000

    Update config.lua

--- discord_perms/config.lua
+++ discord_perms/config.lua
@@ -4,6 +4,6 @@ Config = {
 
 	-- Format: ["Role Nickname"] = "Role ID" You can get role id by doing \@RoleName
 	Roles = {
-		["TestRole"] = "Some Role ID"
+		["TestRole"] = "Some Role ID" -- This would be checked by doing exports.discord_perms:IsRolePresent(user, "TestRole")
 	}
 }
